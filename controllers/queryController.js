2f06af789389d6f1cd7cc6540c76e3bf3ab8cdde
Rahul singh <rahuls6408@gmail.com>
2026-04-05 16:59:46 +0530

    implement query controller

---
+++ controllers/queryController.js
@@ -0,0 +1,30 @@
+import pool from "../db/index.js"; 
+import { getEmbeddings } from "../services/embeddingServices.js";
+import { getAnswer } from "../services/llmServies.js";
+
+const queryDocuments = async (req, res) => {
+  try {
+    const {question}=req.body;
+    if(!question){
+        return res.status(400).json({error:"Question is required"});
+    }
+    const [questionEmbedding]=await getEmbeddings([question]);
+   const result = await pool.query(
+  `SELECT content
+   FROM documents
+   ORDER BY embedding <-> $1
+   LIMIT 5`,
+  [`[${questionEmbedding.join(",")}]`]
+);
+    const context=result.rows.map(row=>row.content);
+    const answer=await getAnswer(question,context);
+    return res.json({
+        answer,
+        sources:result.rows
+    });
+} catch (error) {
+    console.error("Error querying documents:", error);
+    return res.status(500).json({ error: "Internal server error" });
+} 
+};
+export default queryDocuments;
