7d92d28a7d1cb6a28a8fc1ae23f5dee175bb5130
Rahul singh <rahuls6408@gmail.com>
2026-04-05 13:10:04 +0530

    fixed app.js

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config'; 
 import express from 'express';
-import uploadRoutes from './routes/uploadRoutes.js';
 import cors from 'cors';
-
+import uploadRoutes from './routes/uploadRoutes.js';
 import pool from './db/index.js';
 
+const app = express();
+const port = process.env.PORT || 3000;
 
-const app=express();
 app.use(cors());
 app.use(express.json());
-app.use('/api',uploadRoutes);
-const port=3000||process.env.port;
+app.use('/api', uploadRoutes);
+
 app.get("/test-db", async (req, res) => {
   try {
     const result = await pool.query("SELECT NOW()");
@@ -21,6 +20,7 @@ app.get("/test-db", async (req, res) => {
     res.status(500).send("DB error");
   }
 });
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
