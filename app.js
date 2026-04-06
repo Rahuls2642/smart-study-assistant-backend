106c6cc88ba86b6c7598d73aa113be3718484551
Rahul singh <rahuls6408@gmail.com>
2026-04-06 19:02:34 +0530

    added api

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ import express from 'express';
 import cors from 'cors';
 import uploadRoutes from './routes/uploadRoutes.js';
 import pool from './db/index.js';
-
+import queryRoutes from './routes/queryRoutes.js';
+import authRoutes from "./routes/authRoutes.js"
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
 app.use(express.json());
+app.use('/api/auth',authRoutes)
 app.use('/api', uploadRoutes);
+app.use('/api', queryRoutes);
 
 app.get("/test-db", async (req, res) => {
   try {
