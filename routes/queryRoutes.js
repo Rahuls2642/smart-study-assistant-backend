6c1c281d117f532f2c17668d3cc163bccd00fe11
Rahul singh <rahuls6408@gmail.com>
2026-04-06 19:01:28 +0530

    Protect routes

--- routes/queryRoutes.js
+++ routes/queryRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import queryDocs from '../controllers/queryController.js';
+import authMiddleware from '../middleware/auth.js';
 const router=express.Router();
-router.post('/query',queryDocs);
+router.post('/query',authMiddleware,queryDocs);
 export default router;
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import uploadPDF from '../controllers/uploadController.js';
 import multer from 'multer';
+import authMiddleware from '../middleware/auth.js';
 const router=express.Router();
 const storage=multer.memoryStorage();
 const upload=multer({storage});
-router.post('/upload',upload.single('file'),uploadPDF);
+router.post('/upload',upload.single('file'),authMiddleware,uploadPDF);
 export default router;
