d42acde9535b94054edaaba963625ba8c1ee5aae
Rahul singh <rahuls6408@gmail.com>
2026-04-13 12:28:31 +0530

    fix cors

--- app.js
+++ app.js
@@ -8,10 +8,7 @@ import authRoutes from "./routes/authRoutes.js"
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: "http://localhost:5173",
-
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth',authRoutes)
 app.use('/api', uploadRoutes);
