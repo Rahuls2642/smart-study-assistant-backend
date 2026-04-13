747711a5d9f881d79effdca91ce0d3580d3c260c
Rahul singh <rahuls6408@gmail.com>
2026-04-13 10:11:42 +0530

    fixed chuncking logic

--- utils/chunkText.js
+++ utils/chunkText.js
@@ -1,10 +1,8 @@
-const chunkText = (text, size = 500) => {
+const chunkText = (text, size = 1000, overlap = 200) => {
   const chunks = [];
-
-  for (let i = 0; i < text.length; i += size) {
+  for (let i = 0; i < text.length; i += (size - overlap)) {
     chunks.push(text.slice(i, i + size));
   }
-
   return chunks;
 };
 
